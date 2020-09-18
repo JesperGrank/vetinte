ec57ef2370e127d21b9d88d29ae475d36ca1a951
Jesper Grankvist <jesper.grankvist@hotmail.com>
2020-09-18 23:23:03 +0200

    hej

---
+++ automatisk.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function (e){
+    let siffer = document.getElementById("ones")
+    let total = document.getElementById("total")
+
+    siffer.addEventListener("change", function(e){
+        console.log("hej");
+    })
+
+    total.addEventListener("change", function(e){
+        siffer.
+    })
+
+
+})
